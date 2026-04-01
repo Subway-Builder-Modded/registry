55a1945c34834101440eda5f5d9689170b9fe4e1
Alex Kimn <alex.kimn@outlook.com>
2026-04-02 03:10:52 +0900

    update

--- scripts/lib/map-demand-stats/index.ts
+++ scripts/lib/map-demand-stats/index.ts
@@ -1,5 +1,6 @@
 export * from "./types.js";
 export { extractDemandStatsFromZipBuffer } from "./extraction.js";
+export { parseDemandGridData } from "./extraction.js";
 export {
   generateMapDemandStats,
   resolveAndExtractDemandStatsForMapSource,
